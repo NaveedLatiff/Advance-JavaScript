100401ae51e99d24d7969096a2405f269f8ad0be
NaveedLatiff <naveedlatif526@gmail.com>
2025-04-08 16:06:43 +0500

    Spread Operator

---
+++ spread.js
@@ -0,0 +1,23 @@
+// let arr=[1,2,3]
+// Here Spread made the array an object but we can't make object into an array
+// let obj={...arr}
+// console.log(obj)
+
+
+// We can also change the value of object using spread
+// let obj={
+//     name:"Naveed",
+//     email:"naveedlatif526"
+// }
+// But it will not change the name in the original object
+// console.log({...obj,name:"Sarim"})
+
+
+// let arr=[1,4,3]
+// let sum=(a,b,c)=>{
+//     console.log(a+b+c)
+// }
+// Here Spread will give one by one value like 1 then 4 and then 3 it will not give an entire array
+// sum(...arr)
+
+
